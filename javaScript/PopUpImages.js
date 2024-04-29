ebdfcfb621f1940d8bdfad4013f226f7a7ccdda8
JudeRickard <juderickard04@gmail.com>
2024-04-29 18:00:33 +0100

    added new content

--- javaScript/PopUpImages.js
+++ javaScript/PopUpImages.js
@@ -1,25 +1,30 @@
 let originalImage = ""; // Store the original image source
-let newImage = "../images/social_media/EmailMsgVerify.png"; // New image source
+let newImage = ""; // New image source
 let isOriginal = true; // Flag to track current image state
 
 const all_buttons = document.querySelectorAll('.btn');
 console.log(all_buttons) //ensuring all buttons being logged
 
-let buttonClicked = 0;
+let targetImage = "";
+let buttonClicked = "";
 all_buttons.forEach(bt =>{
     bt.addEventListener('click', (e) =>{
-        console.long(e.target);
+        buttonClicked =e.target.id;
+        targetImage =e.target.innerHTML;
+        console.log(buttonClicked);
+        console.log(targetImage);
+        toggleImage();
     })
 });
 
 
 // Function to toggle between original and new image
 function toggleImage() {
-    let img = document.querySelector("#bannerImage1");
+    let img = document.querySelector(targetImage);
 
     if (isOriginal) {
         originalImage = img.getAttribute("src"); // Store the original image
-        img.setAttribute("src", newImage); // Set the new image
+        img.setAttribute("src", buttonClicked); // Set the new image
         isOriginal = false;
     } else {
         img.setAttribute("src", originalImage); // Set the original image
