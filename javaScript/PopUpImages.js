4ea219bdaa890770dfe1005b84d0f534f4426d6b
JudeRickard <159781396+JudeRickard@users.noreply.github.com>
2024-05-01 20:13:30 +0100

    working it out

--- javaScript/PopUpImages.js
+++ javaScript/PopUpImages.js
@@ -2,6 +2,8 @@ let originalImage = "../images/Universal/PlaceHolder.png"; // Store the original
 let newImage = ""; // New image source
 let isOriginal = false; // Flag to track current image state
 
+let src="";
+
 const all_buttons = document.querySelectorAll('.btn');
 console.log(all_buttons) //ensuring all buttons being logged
 
@@ -11,8 +13,10 @@ all_buttons.forEach(bt =>{
     bt.addEventListener('click', (e) =>{
         buttonClicked =e.target.id;
         targetImage =e.target.innerHTML;
-        console.log(buttonClicked);
-        console.log(targetImage);
+
+        var srcCheck = document.getElementById(targetImage).getAttribute('src');
+        console.log("src:",srcCheck);
+
         toggleImage();
     })
 });
@@ -22,20 +26,18 @@ all_buttons.forEach(bt =>{
 function toggleImage() {
     let img = document.querySelector(targetImage);
 
+    if (src == "../images/Universal/PlaceHolder.png") {
+        isOriginal = false;
+        console.log(isOriginal)
+    } else {
+        isOriginal = true;
+        console.log(isOriginal)
+    }
+
     if (isOriginal == true) {
        // originalImage = img.getAttribute("src"); // Store the original image
         img.setAttribute("src", buttonClicked); // Set the new image
-        isOriginal = false;
-        console.log("target image:",targetImage)
-        console.log("original image:",originalImage)
-        console.log("button clicked:",buttonClicked)
-        console.log("isOriginal:",isOriginal)
     } else {
         img.setAttribute("src", originalImage); // Set the original image
-        isOriginal = true;
-        console.log("target image:",targetImage)
-        console.log("original image:",originalImage)
-        console.log("button clicked:",buttonClicked)
-        console.log("isOriginal:",isOriginal)
     }
 }
