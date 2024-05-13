9a8ebcd3f675f67f7328f4433056a8df293f2e07
JudeRickard <juderickard04@gmail.com>
2024-05-13 03:07:43 +0100

    Update PopUpImages.js

--- javaScript/PopUpImages.js
+++ javaScript/PopUpImages.js
@@ -3,6 +3,7 @@ let newImage = ""; // New image source
 let isOriginal = false; // Flag to track current image state
 
 let src="";
+let imageUrl ="";
 
 const all_buttons = document.querySelectorAll('.btn');
 console.log(all_buttons) //ensuring all buttons being logged
@@ -13,10 +14,22 @@ all_buttons.forEach(bt =>{
     bt.addEventListener('click', (e) =>{
         buttonClicked =e.target.id;
         targetImage =e.target.innerHTML;
-
-        var srcCheck = document.getElementById(targetImage).getAttribute('src');
-        console.log("src:",srcCheck);
-
+ 
+        window.addEventListener('DOMContentLoaded', (event) => {
+            // Get the image element by id
+            const image = document.getElementById(targetImage);
+            
+            // Check if the image element is not null before accessing its src attribute
+            if (image) {
+                // Get the src attribute value and save it as a variable
+                const imageUrl = image.src;
+                
+                // Display the image src in the console
+                console.log("Image Source:", imageUrl);
+            } else {
+                console.log("Image element not found");
+            }
+        });
         toggleImage();
     })
 });
@@ -26,7 +39,7 @@ all_buttons.forEach(bt =>{
 function toggleImage() {
     let img = document.querySelector(targetImage);
 
-    if (src == "../images/Universal/PlaceHolder.png") {
+    if (imageUrl == "../images/Universal/PlaceHolder.png") {
         isOriginal = false;
         console.log(isOriginal)
     } else {
@@ -41,3 +54,4 @@ function toggleImage() {
         img.setAttribute("src", originalImage); // Set the original image
     }
 }
+
