d78690362eabfb954ae5f6d9948ccd7532d9aeba
JudeRickard <159781396+JudeRickard@users.noreply.github.com>
2024-05-13 13:18:28 +0100

    actually fixed

--- javaScript/PopUpImages.js
+++ javaScript/PopUpImages.js
@@ -1,3 +1,10 @@
+
+div1.style.display = "none";
+div2.style.display = "none";
+div3.style.display = "none";
+div4.style.display = "none";
+div5.style.display = "none";
+
 function toggleVisibility(divId) {
         var div = document.getElementById(divId);
         if (div.style.display === "none") {
